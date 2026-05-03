36c3df40e014a6fc5646d9c1c47c77e3bbd5de47
Lesyk <lesuk93@yahoo.com>
2026-05-03 19:38:23 +0300

    Update resolve_start_end_from_range

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,12 +1,11 @@
 def resolve_start_end_from_range(
     range_value: list[int] | None, all_counted: int
 ) -> tuple[int, int]:
+    max_index = max(0, all_counted - 1)
     if range_value:
-        start = range_value[0]
-        end = (
-            range_value[1] if range_value[1] < all_counted else max(all_counted - 1, 0)
-        )
+        start = min(max(0, range_value[0]), max_index)
+        end = min(max(range_value[1], start), max_index)
     else:
         start = 0
-        end = max(all_counted - 1, 0)
+        end = max_index
     return (start, end)
